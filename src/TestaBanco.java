bb23e6f394db51edbd1ea10044d149002e3ae58a
Caio Tracera <ctrcra@gmail.com>
2021-02-09 07:31:31 -0300

    feat: create a new instance of Cliente by setting the Conta.titular

--- src/TestaBanco.java
+++ src/TestaBanco.java
@@ -1,13 +1,12 @@
 public class TestaBanco {
     public static void main(String[] args) {
-        Cliente primeiroCliente = new Cliente();
-        primeiroCliente.nome = "Caio Tracera";
-        primeiroCliente.cpf = "222.222.222-22";
-        primeiroCliente.profissao = "Programador";
-
         Conta contaDoCaio = new Conta();
+
+        contaDoCaio.titular = new Cliente();
+        contaDoCaio.titular.nome = "Caio Tracera";
+        contaDoCaio.titular.cpf = "222.222.222-22";
+        contaDoCaio.titular.profissao = "Programador";
         contaDoCaio.deposita(100);
-        contaDoCaio.titular = primeiroCliente;
 
         System.out.println(contaDoCaio.titular.nome);
     }
