96bdda0ee139ad68cd2286089fc04bc398edf504
Caio Tracera <ctrcra@gmail.com>
2021-02-09 08:53:30 -0300

    feat: changed getBonificacao

--- src/Gerente.java
+++ src/Gerente.java
@@ -10,7 +10,7 @@ public class Gerente extends Funcionario {
     }
 
     public double getBonificacao() {
-        return super.getSalario();
+        return super.getBonificacao() + super.getSalario();
     }
 
     public boolean autentica(int senha) {
