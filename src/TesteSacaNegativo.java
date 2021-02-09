0f8e37e0d54d1f41b1ecb2af628daca213ec02ac
Caio Tracera <ctrcra@gmail.com>
2021-02-09 07:38:21 -0300

    feat: add class to test methods

--- src/TesteSacaNegativo.java
+++ src/TesteSacaNegativo.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class TesteSacaNegativo {
+public class TesteSacaNegativo {
+    public static void main(String[] args) {
+        Conta conta = new Conta();
+        conta.deposita(100);
+        conta.saca(200);
+    }
 }
