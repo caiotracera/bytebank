b94d66efda9933e70cd4f0cad7d22f152d339685
Caio Tracera <ctrcra@gmail.com>
2021-02-09 08:13:50 -0300

    refactor: delete unused classes

--- src/TestaValores.java
+++
@@ -1,3 +0,0 @@
-public class TestaValores {
-    Conta conta = new Conta(2, 25);
-}
--- src/TesteSacaNegativo.java
+++
@@ -1,7 +0,0 @@
-public class TesteSacaNegativo {
-    public static void main(String[] args) {
-        Conta conta = new Conta(2, 25);
-        conta.deposita(100);
-        conta.saca(200);
-    }
-}
