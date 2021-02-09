c196206374a303c1eccb504e573f463b1fcbaf04
Caio Tracera <ctrcra@gmail.com>
2021-02-09 08:41:31 -0300

    feat: add class Gerente that extends Funcionario

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Gerente {
+public class Gerente extends Funcionario {
+    private int senha;
+
+    public Gerente(String nome, String cpf, double salario) {
+        super(nome, cpf, salario);
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public double getBonificacao() {
+        return this.getSalario();
+    }
+
+    public boolean autentica(int senha) {
+        if (this.senha == senha) {
+            return true;
+        }
+        return false;
+    }
 }
