02fa779696a43b08d5da6edd53b206facd2ec73d
Caio Tracera <ctrcra@gmail.com>
2021-02-09 08:19:56 -0300

    feat: add constructor

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -3,6 +3,12 @@ public class Funcionario {
     private String cpf;
     private double salario;
 
+    public Funcionario(String nome, String cpf, double salario) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salario = salario;
+    }
+
     public String getNome() {
         return nome;
     }
