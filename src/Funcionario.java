698f5e9ca983b68fd984847e0d1bfef6c98df224
Caio Tracera <ctrcra@gmail.com>
2021-02-09 08:22:53 -0300

    feat: add attribute cargo

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -2,6 +2,7 @@ public class Funcionario {
     private String nome;
     private String cpf;
     private double salario;
+    private int cargo = 0; // 0 - Funcionario comum, 1 - Gerente, 2 - Diretor
 
     public Funcionario(String nome, String cpf, double salario) {
         this.nome = nome;
@@ -31,6 +32,14 @@ public class Funcionario {
 
     public void setSalario(double salario) {
         this.salario = salario;
+    }
+
+    public int getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(int cargo) {
+        this.cargo = cargo;
     }
 
     public double getBonificacao() {
