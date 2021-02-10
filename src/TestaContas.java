c8825f5615597bdd2f45afd9aa50d2b3b538e071
Caio Tracera <ctrcra@gmail.com>
2021-02-10 09:12:35 -0300

    feat: add new types of contas

---
+++ src/TestaContas.java
@@ -0,0 +1,12 @@
+public class TestaContas {
+    public static void main(String[] args) {
+        ContaCorrente cc = new ContaCorrente(111,111);
+        cc.deposita(100);
+
+        ContaPoupanca cp = new ContaPoupanca(222,222);
+        cp.deposita(200);
+
+        cc.transfere(10, cp);
+        System.out.println(cc.getSaldo() + " / " + cp.getSaldo());
+    }
+}
